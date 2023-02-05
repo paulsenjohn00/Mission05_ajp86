45829e71a2d88e4b3328869262d648d7f213364f
paulsenjohn00 <paulsenjohn00@gmail.com>
2023-02-05 14:53:52 -0700

    new app

--- AboutMeWebApp/Startup.cs
+++ AboutMeWebApp/Startup.cs
@@ -16,13 +16,28 @@ namespace AboutMeWebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsEnvironment("Development"))
+            { 
+                app.UseDeveloperExceptionPage(); 
+            }
+            
             app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
